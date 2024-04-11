bbad77185c803cda0a57f8e76763b184dd6a8e5f
Zitlaly Lora Gonzalez <zitlalyloragonzalez@Zitlalys-MacBook-Pro.local>
2024-04-11 12:27:57 -0400

    Encoder and Main 3

--- main.py
+++ main.py
@@ -10,12 +10,7 @@ def encode(password):
         encode_digits = str((int(digit)+ 3) % 10)
         encoded_password = encode_digits
     return encoded_password
-def decode(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        encode_digits = str((int(digit)- 3) % 10)
-        decoded_password = encode_digits
-    return decoded_password
+
 
 def main():
     while True:
@@ -26,10 +21,7 @@ def main():
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your encoded password is:", encoded_password)
-        elif choice == 2:
-            encoded_password = input("Please enter your password to decode: ")
-            decoded_password = decode(encoded_password)
-            print("Your decoded password is:", decoded_password)
+
         elif choice == 3:
             break
         else:
