b007e3cad3b3135759115cc739b3b4cdf772f4fc
Allie Simpson <alliesimpson@ufl.edu>
2024-04-11 12:39:47 -0400

    New main with the decoder function

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def encode(password):
         encode_digits = str((int(digit)+ 3) % 10)
         encoded_password = encode_digits
     return encoded_password
-
+def decode(password):
+	decoded_password =" "
+	for digit in encoded_password:
+		encoded_digits = str((int(digit)- 3) % 100
+		decoded_password = encode_digits
+	return decoded_password
 
 def main():
     while True:
@@ -20,12 +25,15 @@ def main():
         if choice == 1:
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
-            print("Your encoded password is:", encoded_password)
+            print("Your password has been encoded and stored")
+	elif choice == 2:
+		encoded_pasword = input("Please enter your password to decode: ")
+		decoded_password = decode(encoded_password)
+		print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
 
         elif choice == 3:
             break
-        else:
-            print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
